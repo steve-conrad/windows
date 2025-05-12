093b3a51ba6dea956d9f51a6524dab004de68875
steve-conrad <steve.conrad14@gmail.com>
2025-05-12 14:06:48 -0400

    added script

---
+++ manage-calendar-permissions.ps1
@@ -0,0 +1,19 @@
+﻿#Manage User Calendar Specific Permissions
+
+#Set execution policy to import exchange module
+Set-ExecutionPolicy RemoteSigned -Force
+
+#Import Exchange module
+Import-Module ExchangeOnlineManagement
+
+#Connect to exchange
+Connect-ExchangeOnline
+
+#View Calendar permissions
+Get-MailboxFolderPermission -Identity username:\Calendar
+
+#Add Calendar permissions
+Add-MailboxFolderPermission -Identity username@domain.com:\calendar -user username@domain.com -AccessRights Editor
+
+#Remove Calendar permissions
+Remove-MailboxfolderPermission -Identity username@domain.com:\calendar -user username@domain.com
