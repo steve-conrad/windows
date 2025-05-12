221cc1a89bbd3d1d7c088f1cd6da8f6d452c2c93
steve-conrad <steve.conrad14@gmail.com>
2025-05-12 13:46:51 -0400

    added script

---
+++ azuread-activeuser-csv-compare.ps1
@@ -0,0 +1,14 @@
+﻿### Compare CSV user list to Azure AD to audit active members ###
+
+Connect-AzureAD
+
+$Users = Import-Csv -Path "C:\Scripts\userlist.csv"
+
+ForEach ($User in $Users) 
+    {
+    $UserInfo = Get-AzureADUser -SearchString $user.displayName
+    Write-Host $UserInfo.displayName
+    }
+$EnabledADUsers = Get-AzureADUser -All $true -Filter "AccountEnabled eq true"
+
+Compare-Object -ReferenceObject $Users -DifferenceObject $EnabledADUsers -Property Displayname
